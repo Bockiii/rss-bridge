b75fc2151d1b2bcea14db1746d0ae68d5b071919
Bocki <henning@bocklage.com>
2023-03-02 16:07:22 +0000

    update

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,16 @@
+apt-get update && \
+    apt-get install --yes --no-install-recommends \
+      nginx \
+      zlib1g-dev \
+      libzip-dev \
+      libmemcached-dev \
+      nss-plugin-pem \
+      libicu-dev && \
+    docker-php-ext-install zip && \
+    docker-php-ext-install intl && \
+    pecl install memcached && \
+    docker-php-ext-enable memcached && \
+    docker-php-ext-enable opcache && \
+    mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+
+cp "$HOME/.devcontainer/xdebug.ini" "$PHP_INI_DIR/xdebug.ini"

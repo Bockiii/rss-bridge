f842f373738e7996b1f513ba9247333081d3f259
Bocki <henning@bocklage.com>
2023-03-02 19:02:39 +0000

    adasd

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,17 +1,16 @@
-apt-get update && \
-    apt-get install --yes --no-install-recommends \
+sudo apt-get update
+sudo apt-get install --yes --no-install-recommends \
       nginx \
       zlib1g-dev \
       libzip-dev \
       libmemcached-dev \
       nss-plugin-pem \
-      libicu-dev && \
-    docker-php-ext-install zip && \
-    docker-php-ext-install intl && \
-    pecl install memcached && \
-    docker-php-ext-enable memcached && \
-    docker-php-ext-enable opcache && \
-    mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
-
-cp "$HOME/.devcontainer/xdebug.ini" "$PHP_INI_DIR/xdebug.ini"
-cp "$HOME/config/nginx.conf" "/etc/nginx/sites-enabled/default"
+      libicu-dev
+sudo docker-php-ext-install zip
+sudo docker-php-ext-install intl
+sudo pecl install memcached
+sudo docker-php-ext-enable memcached
+sudo docker-php-ext-enable opcache
+sudo mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+sudo cp "$HOME/.devcontainer/xdebug.ini" "$PHP_INI_DIR/xdebug.ini"
+sudo cp "$HOME/.devcontainer/nginx.conf" "/etc/nginx/sites-enabled/default"

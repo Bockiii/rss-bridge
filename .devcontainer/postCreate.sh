24d8bc323e34a44d6c053d06eddc7d00e9d8cc00
Dedy Martadinata S <dedyms@proton.me>
2023-03-03 15:10:04 +0000

    use apache, add xdebug-info for test

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,20 +1,17 @@
 cd /workspaces/rss-bridge
 
-apt-get update && \
-    apt-get install --yes --no-install-recommends \
+sudo apt-get update
+sudo apt-get install --yes --no-install-recommends \
       nginx \
       zlib1g-dev \
       libzip-dev \
       nss-plugin-pem \
       libicu-dev && \
-    docker-php-ext-install zip && \
-    docker-php-ext-install intl && \
-    docker-php-ext-enable opcache && \
-    mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
-    
-cp ./.devcontainer/nginx.conf /etc/nginx/sites-enabled/default
-cp ./.devcontainer/xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini
+docker-php-ext-install zip && \
+docker-php-ext-install intl && \
+docker-php-ext-enable opcache && \
+sudo mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+#sudo cp ./.devcontainer/nginx.conf /etc/nginx/sites-enabled/default
+sudo cp ./.devcontainer/xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini
 
-nginx
-
-php-fpm
+sudo apache2ctl start

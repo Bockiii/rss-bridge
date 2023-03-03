b18a47fe8c22a421d3a6a5d87a0646fcbb43d6b0
Bocki <henning@bocklage.com>
2023-03-03 01:33:44 +0000

    dd

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,17 +1,17 @@
-sudo apt-get update
-sudo apt-get install --yes --no-install-recommends \
-      nginx \
-      zlib1g-dev \
-      libzip-dev \
-      libmemcached-dev \
-      nss-plugin-pem \
-      libicu-dev
-sudo export APACHE_DOCUMENT_ROOT=/workspaces/rss-bridge
-sudo docker-php-ext-install zip
-sudo docker-php-ext-install intl
-sudo pecl install memcached
-sudo docker-php-ext-enable memcached
-sudo docker-php-ext-enable opcache
+# sudo apt-get update
+# sudo apt-get install --yes --no-install-recommends \
+#       nginx \
+#       zlib1g-dev \
+#       libzip-dev \
+#       libmemcached-dev \
+#       nss-plugin-pem \
+#       libicu-dev
+# sudo export APACHE_DOCUMENT_ROOT=/workspaces/rss-bridge
+# sudo docker-php-ext-install zip
+# sudo docker-php-ext-install intl
+# sudo pecl install memcached
+# sudo docker-php-ext-enable memcached
+# sudo docker-php-ext-enable opcache
 sudo mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
 sudo cp "/workspaces/rss-bridge/.devcontainer/xdebug.ini" "$PHP_INI_DIR/xdebug.ini"
 sudo sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf

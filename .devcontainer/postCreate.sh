cfe1c6fe6e203ab06a845fd78d3a24797d5e1d90
Bocki <henning@bocklage.com>
2023-03-03 10:30:08 +0100

    ggag

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,17 +1,22 @@
-# sudo apt-get update
-# sudo apt-get install --yes --no-install-recommends \
-#       nginx \
-#       zlib1g-dev \
-#       libzip-dev \
-#       libmemcached-dev \
-#       nss-plugin-pem \
-#       libicu-dev
-# sudo export APACHE_DOCUMENT_ROOT=/workspaces/rss-bridge
-# sudo docker-php-ext-install zip
-# sudo docker-php-ext-install intl
-# sudo pecl install memcached
-# sudo docker-php-ext-enable memcached
-# sudo docker-php-ext-enable opcache
-sudo chmod a+x \"$(pwd)\" && sudo rm -rf /var/www/html && sudo ln -s \"$(pwd)\" /var/www/html
-sudo mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
-sudo cp "/workspaces/rss-bridge/.devcontainer/xdebug.ini" "$PHP_INI_DIR/xdebug.ini"
+cd /workspaces/rss-bridge
+
+apt-get update && \
+    apt-get install --yes --no-install-recommends \
+      nginx \
+      zlib1g-dev \
+      libzip-dev \
+      libmemcached-dev \
+      nss-plugin-pem \
+      libicu-dev && \
+    docker-php-ext-install zip && \
+    docker-php-ext-install intl && \
+    pecl install memcached && \
+    docker-php-ext-enable memcached && \
+    docker-php-ext-enable opcache && \
+    mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+    
+cp ./.devcontainer/nginx.conf /etc/nginx/sites-enabled/default
+
+nginx
+
+php-fpm

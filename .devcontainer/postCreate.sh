4eff099401f0abb2be022217a7bad655cd358c92
Bocki <henning@bocklage.com>
2023-03-03 10:05:50 +0000

    ff

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,18 +5,10 @@ apt-get update && \
       nginx \
       zlib1g-dev \
       libzip-dev \
-      libmemcached-dev \
       nss-plugin-pem \
-      gcc \
-      make \
-      autoconf \
-      libc-dev \
-      pkg-config \
       libicu-dev && \
     docker-php-ext-install zip && \
     docker-php-ext-install intl && \
-    pecl install memcached && \
-    docker-php-ext-enable memcached && \
     docker-php-ext-enable opcache && \
     mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
     
